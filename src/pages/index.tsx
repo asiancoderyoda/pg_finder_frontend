4ac21145714e91d2012aa5545876e8638bb76fe7
asiancoderyoda <avigyanbhaktacontai@gmail.com>
2021-12-19 21:21:43 +0530

    project setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
 import { Button, Container, Box, Theme } from '@mui/material';
-import { createStyles, makeStyles } from '@mui/styles';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100vh',
-    },
-  })
-);
-
 const Home: NextPage = () => {
-  const classes = useStyles();
   return (
     <React.Fragment>
       <Container>
-        <Box className={classes.root}>
+        <Box>
           <Button disableElevation color="primary" variant="contained">
             Click Me
           </Button>
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,9 +1,38 @@
+/** @jsxRuntime classic /
+/* @jsx jsx */
+import { css, jsx } from '@emotion/react';
+import { Box, Button, Typography } from '@mui/material';
+import { GetServerSidePropsContext } from 'next';
 import React from 'react';
+
+const buttonStyle = css({
+  height: '100vH',
+  width: '100vW',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
 
 interface Props {}
 
 const Register: React.FC<Props> = ({}) => {
-  return <div>Register</div>;
+  return (
+    <React.Fragment>
+      <Box css={buttonStyle}>
+        <Typography variant="h4">Welcome to the server!</Typography>
+        <Button type="button" variant="contained" color="primary">
+          Server Rendered Button
+        </Button>
+      </Box>
+    </React.Fragment>
+  );
+};
+
+export const getServerSideProps = (ctx: GetServerSidePropsContext) => {
+  return {
+    props: {},
+  };
 };
 
 export default Register;
